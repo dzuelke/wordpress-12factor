b0473372c92a29d5f6eea948957430956507fb15
David Zuelke <dzuelke@gmail.com>
2017-02-19 21:06:56 +0100

    fix nginx config tabs

--- nginx-wordpress.conf.php
+++ nginx-wordpress.conf.php
@@ -45,5 +45,5 @@ rewrite /wp-admin$ $scheme://$host$uri/ permanent;
 
 # Directives to send expires headers and turn off 404 error logging.
 location ~* ^.+\.(ogg|ogv|svg|svgz|eot|otf|woff|mp4|ttf|rss|atom|jpg|jpeg|gif|png|ico|zip|tgz|gz|rar|bz2|doc|xls|exe|ppt|tar|mid|midi|wav|bmp|rtf)$ {
-       access_log off; log_not_found off; expires max;
+	access_log off; log_not_found off; expires max;
 }
